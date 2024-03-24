8f55a5698ff062d0aa3c9e19df2e29deeabe47bd
Vlad Udrescu <vladudrescu@MacBook-Pro-14-Vlad.local>
2024-03-24 17:42:00 +0200

    render a 404 response with a rendered template instead of redirect in the get method

--- orderPizza/views.py
+++ orderPizza/views.py
@@ -1,11 +1,13 @@
 from typing import Any
 from django.db.models.base import Model as Model
-from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from .models import *
 from django.db.models.query import QuerySet
 from django.views.generic import ListView, DetailView
 from django.template.loader import render_to_string
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 PIZZA_TYPE = ['Joshua', 'Happy']
@@ -14,25 +16,29 @@ class ViewPizzaMenu(ListView):
     model = Pizza
     context_object_name = 'pizzas'
     template_name = 'orderPizza/pizza_menu.html'
-    def get_queryset(self) -> QuerySet[any]                                       :
+    def get_queryset(self) -> QuerySet[any]:
         return Pizza.objects.all()
 
 class DetailedViewPizza(DetailView):
     model = Pizza
     context_object_name = 'pizza'
-    def get_object(self):
-        return Pizza.objects.get(pk=self.kwargs['pk'])
+    def get(self, request, *args, **kwargs):
+        try:
+            return super().get(request, *args, **kwargs)
+        except Http404:
+            rendered_template = render_to_string('orderPizza/404.html', {'error_message': 'This pizza index is not valid'})
+            return HttpResponseNotFound(rendered_template)
     
 class FilterPizzaMenu(ListView):
     model = Pizza
     context_object_name = 'pizzas'
     template_name = 'orderPizza/pizza_menu.html'
-    def dispatch(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         if not self.kwargs['pizzatype'] in PIZZA_TYPE:
-            rendered_template = render_to_string('orderPizza/404.html', {'error_message': 'This pizza type is not valid'})
+            rendered_template = render_to_string('orderPizza/404.html', {'error_message': 'This pizza filter is not valid'})
             return HttpResponseNotFound(rendered_template)
         else:
-            return super().dispatch(request, *args, **kwargs)
+            return super().get(request, *args, **kwargs)
     def get_queryset(self) -> QuerySet[Any]:
         return Pizza.objects.filter(pizza_type=self.kwargs['pizzatype'])
 
@@ -46,7 +52,9 @@ class ViewDailyMenu(ListView):
 class DetailedViewDailyMenu(DetailView):
     model = DailyMenu
     context_object_name = 'dailyMenu'
-    def get_object(self):
-        return DailyMenu.objects.get(pk=self.kwargs['pk'])
-
-    
+    def get(self, request, *args, **kwargs):
+        try:
+            return super().get(request, *args, **kwargs)
+        except Http404:
+            rendered_template = render_to_string('orderPizza/404.html', {'error_message': 'This daily meny is not valid'})
+            return HttpResponseNotFound(rendered_template)
